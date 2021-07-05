576bc9c8295ca6e0cdb2e48cfd375b03317e8400
Sergey <borzsonite@gmail.com>
2021-07-05 07:05:50 +0300

    33.53

---
+++ src/main/java/edu/sergeyrozhkov/springsecuritydemo/model/Role.java
@@ -0,0 +1,6 @@
+package edu.sergeyrozhkov.springsecuritydemo.model;
+
+public enum Role {
+    ADMIN,
+    USER;
+}
---
+++ src/main/java/edu/sergeyrozhkov/springsecuritydemo/rest/DeveloperRestControllerV1.java
@@ -0,0 +1,45 @@
+package edu.sergeyrozhkov.springsecuritydemo.rest;
+
+import edu.sergeyrozhkov.springsecuritydemo.model.Developer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestControllerV1 {
+
+
+    List<Developer> DEVELOPERS = Stream.of(
+            new Developer(1L, "Ivan", "Ivanov"),
+            new Developer(2L, "Petr", "Petrov"),
+            new Developer(3L, "Sergey", "Sergeev")
+    ).collect(Collectors.toList());
+
+
+    @GetMapping()
+    public List<Developer> getAll() {
+        return DEVELOPERS;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return DEVELOPERS.stream()
+                .filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PostMapping
+    public Developer create(@RequestBody Developer developer) {
+        DEVELOPERS.add(developer);
+        return developer;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable int id) {
+        DEVELOPERS.removeIf(developer -> developer.getId().equals(id));
+    }
+}
